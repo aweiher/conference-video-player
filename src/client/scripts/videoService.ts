b823a4b398bda37eafe72126bb09a71a2390b85d
defel <defel@users.noreply.github.com>
2015-12-22 18:33:27 +0100

    feature: load video-data from api

--- src/client/scripts/videoService.ts
+++ src/client/scripts/videoService.ts
@@ -1,21 +1,15 @@
 import {Injectable} from 'angular2/core';
 import {IVideo, IVideoService} from "./interfaces";
 import {VideoRestService} from "./videoRestService";
+import {Response} from 'angular2/http';
 
 @Injectable()
-export class VideoService implements IVideoService {
-    private videos: IVideo[];
-
-    constructor (public restService: VideoRestService) {
-    };
-
-    fetchAll() {
-        return this.restService.getAll().subscribe((videos:IVideo[]) => {
-            this.videos = videos;
-        });
-    }
+export class VideoService implements IVideoService{
+    constructor (public restService: VideoRestService) { };
 
     getAll() {
-        return this.videos;
+        return this.restService
+            .getAll()
+            .map( res => res.json())
     }
 }
