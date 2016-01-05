43a025e408c5e6f07a27b78a5f83d9d8566bb6b5
defel <defel@users.noreply.github.com>
2016-01-05 23:09:53 +0100

    feat: added the /data endpoint (close #12)

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,65 +10,12 @@ let server = new Hapi.Server(''),
 server.connection({port: 3000});
 server.register(require('inert'), function () {});
 
-
-server.route({
-    method: "GET",
-    path: "/api/channels",
-    handler: (req, reply) => {
-
-        fs.readdir(`${dataPath}/channels/`, (err, items) => {
-            reply(items.map(
-                (fileName) => JSON.parse(fs.readFileSync(`${dataPath}/channels/${fileName}`).toString())
-            ));
-        });
-
-        //let filePath = `${dataPath}/channels/${req.params['channel_id']}.json`;
-        //
-        //if(!fs.existsSync(filePath)) {
-        //    return reply(Boom.notFound("Channel does not exists"));
-        //}
-        //
-        //fs.readFile(filePath, (err, buf) => {
-        //    if(err) {
-        //        console.error('unexpected error: ', err);
-        //        return reply(Boom.badImplementation("any error"));
-        //    }
-        //
-        //    reply(buf.toString());
-        //})
-    }
-});
-
-server.route({
-    method: "GET",
-    path: "/api/channels/{channel_id}",
-    handler: (req, reply) => {
-        let filePath = `${dataPath}/channels/${req.params['channel_id']}.json`;
-
-        if(!fs.existsSync(filePath)) {
-            return reply(Boom.notFound("Channel does not exists"));
-        }
-
-        fs.readFile(filePath, (err, buf) => {
-            if(err) {
-                console.error('unexpected error: ', err);
-                return reply(Boom.badImplementation("any error"));
-            }
-
-            reply(buf.toString());
-        })
-    }
-});
-
 server.route({
     method: 'GET',
-    path: '/api/videos',
+    path: '/data',
     handler: (req, reply) => {
-        reply([{
-            url: "moep"
-        }, {
-            url: "miep"
-        }]);
+        let data = require('../../src/data/data.json');
+        reply(data);
     }
 });
 
